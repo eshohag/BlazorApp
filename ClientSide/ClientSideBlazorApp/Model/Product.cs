dde092fbde1f63a19e034a15743faec8b5c339f2
Md. Shohag Mia <shohag.mia@datasoft-bd.com>
2019-12-31 11:44:21 +0600

    Add project files.

---
+++ ClientSide/ClientSideBlazorApp/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientSideBlazorApp.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ ServerSide/BlazorApp.ClassLibrary/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorApp.ClassLibrary.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+}
